c48898ddac064a6036f2d67498915d1df66ae8ad
NiyaRai13 <niyarai2963@gmail.com>
2025-07-15 21:57:08 +0545

    operations

--- myproject/myapp/views/main_views.py
+++ myproject/myapp/views/main_views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from myapp.models import Task
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 
 def index(request):
     data = Task.objects.all()
@@ -20,13 +21,20 @@ def createlist(request):
     return render(request, 'main/create_list.html')
 
 def editlist(request, id):
-    task = Task.objects.get( id=id)
+    task= Task.objects.get(id=id)
+    previous_task = Task.objects.get( id=id)
     if request.method == 'POST':
-        task.title = request.POST.get('title')
-        task.description = request.POST.get('description')
-        task.save()
-        return redirect('index')
-    return render(request, 'main/edit.html', {'task': task})
+        title = request.POST.get('title')
+        description = request.POST.get('description')
+        previous_task.save()
+        if task.author != request.user:
+            return render(request, 'main/editblog.html', {'error': 'You are not authorized to edit this blog.'})
+        if previous_task.author != request.user:
+            title = previous_task.title
+            description = previous_task.description
+            previous_task.save()
+        return redirect('index',id=id)
+    return render(request, 'main/edit.html', {'task': previous_task})
 
 def deletelist(request, id):
     task=Task.objects.filter(id=id)
